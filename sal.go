00af7d010387d455721ef56eba61151ed7b9a701
Zaur Abasmirzoev <i@zaur.io>
2018-08-10 13:28:46 +0300

    Add utils for converting camel case to underscore

--- sal.go
+++ sal.go
@@ -3,6 +3,8 @@ package sal
 import (
 	"fmt"
 	"regexp"
+	"strings"
+	"unicode"
 )
 
 var reQueryArgs = regexp.MustCompile("@[a-z]+")
@@ -18,4 +20,39 @@ func QueryArgs(query string) (string, []string) {
 		return fmt.Sprintf("$%d", len(args))
 	})
 	return pgQuery, args
+}
+
+// CamelCaseToUnderscore converts from camel case form to underscore separated form.
+// Ex.: MyFunc => my_func
+func CamelCaseToUnderscore(str string) string {
+	var output []rune
+	var segment []rune
+	for _, r := range str {
+
+		// not treat number as separate segment
+		if !unicode.IsLower(r) && string(r) != "_" && !unicode.IsNumber(r) {
+			output = addSegment(output, segment)
+			segment = nil
+		}
+		segment = append(segment, unicode.ToLower(r))
+	}
+	output = addSegment(output, segment)
+	return string(output)
+}
+
+func addSegment(inrune, segment []rune) []rune {
+	if len(segment) == 0 {
+		return inrune
+	}
+	if len(inrune) != 0 {
+		inrune = append(inrune, '_')
+	}
+	inrune = append(inrune, segment...)
+	return inrune
+}
+
+// UnderscoreToCamelCase converts from underscore separated form to camel case form.
+// Ex.: my_func => MyFunc
+func UnderscoreToCamelCase(s string) string {
+	return strings.Replace(strings.Title(strings.Replace(strings.ToLower(s), "_", " ", -1)), " ", "", -1)
 }
